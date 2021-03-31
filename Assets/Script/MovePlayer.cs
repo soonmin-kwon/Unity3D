cfa4d8bb738995ad0eec6fe16b0be78f71e97fbd
soonmin-kwon <tnsals0709@naver.com>
2021-03-31 23:33:26 +0900

    Map Import, Cinemachine Test, Character Import
    시네머신으로 카메라 뷰 테스트, 사용할 캐릭터, 맵 임포트

--- Assets/Script/MovePlayer.cs
+++ Assets/Script/MovePlayer.cs
@@ -3,15 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
-{
-    private float speed = 5.0f;
-    [SerializeField]
-    private Camera camera;
-    Transform dir;
-    void Awake()
-    {
-        dir = camera.transform;
-    }
+{    
+    private float speed = 5.0f; 
+    Transform dir;    
     void Update()
     {
         PlayerMove();
@@ -25,4 +19,5 @@ public class MovePlayer : MonoBehaviour
         transform.Translate(dir.right * Time.deltaTime * speed * horizontal, Space.World);
         transform.Translate(dir.forward * Time.deltaTime * speed * vertical, Space.World);
     }
+
 }
